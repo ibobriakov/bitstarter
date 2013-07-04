f08f276c8323405e317abaa27d04a9b54e02dc6a
Igor <igor@igor-VirtualBox.(none)>
2013-07-04 23:51:14 +0300

    Next version

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var readStream = fs.createReadStream('index.html');
+  readStream.pipe(response);
+
+
 });
 
 var port = process.env.PORT || 5000;
